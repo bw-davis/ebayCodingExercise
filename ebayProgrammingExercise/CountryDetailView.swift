da949e62052f8ff7b7d456ff1d0a3b71fc194b4c
Brian Davis <bwdavis11@gmail.com>
2017-01-22 19:26:27 -0800

    finished project using interface builder

--- ebayProgrammingExercise/CountryDetailView.swift
+++ ebayProgrammingExercise/CountryDetailView.swift
@@ -11,60 +11,45 @@ import MapKit
 import CoreLocation
 
 class CountryDetailViewController: UIViewController, MKMapViewDelegate {
+    
     @IBOutlet var nameLabelOutlet: UILabel?
-    @IBOutlet var capitalLabelOutlet: UILabel?
-    @IBOutlet var populationLabelOutlet: UILabel?
     @IBOutlet var mapViewOutlet: MKMapView!
-    var pointAnnotation: MKPointAnnotation!
-    var pinAnnotationView: MKPinAnnotationView!
-    var theCountry = Country(name: "", capital: "", population: 0, area: 0.0, latitude: 0.0, longitude: 0.0)
+    
+    /*The selected country created initially with default values but set in prior view controller's prepare for segue method*/
+    var selectedCountry = Country(name: "", capital: "", population: 0, area: 0.0, latitude: 0.0, longitude: 0.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*let countryAnnotation = CountryAnnotation(title: self.theCountry.name, coordinate: CLLocationCoordinate2D(latitude: self.theCountry.latitude, longitude: self.theCountry.longitude), info: "The population is \(String(self.theCountry.population)) and it's capital city is \(self.theCountry.capital)")*/
-        //mapViewOutlet.addAnnotation(countryAnnotation)
         
+        var pointAnnotation: MKPointAnnotation!
+        var pinAnnotationView: MKPinAnnotationView!
         
+        pointAnnotation = MKPointAnnotation()
+        /*set coordinates of point annotation to the lat and long of selected country*/
+        pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: self.selectedCountry.latitude, longitude: self.selectedCountry.longitude)
+        pinAnnotationView = MKPinAnnotationView(annotation: pointAnnotation, reuseIdentifier: nil)
+        mapViewOutlet.centerCoordinate = pointAnnotation.coordinate     //Center the view on the annotation
         
-        self.pointAnnotation = MKPointAnnotation()
-        
-        self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: self.theCountry.latitude, longitude:     self.theCountry.longitude)
-        
-        
-        self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
-        
-        self.mapViewOutlet.centerCoordinate = self.pointAnnotation.coordinate
         var region: MKCoordinateRegion
-        if let countryArea = self.theCountry.area {
-            //region = MKCoordinateRegionMakeWithDistance(
-              //  self.pointAnnotation.coordinate, (countryArea/1.5), (countryArea/1.5))
-            region = MKCoordinateRegion(center: self.pointAnnotation.coordinate, span: MKCoordinateSpan(latitudeDelta: 15, longitudeDelta: 15))
+        /*a few countries have nil for area in those cases I don't span out as far otherwise area is barely visible*/
+        if let countryArea = self.selectedCountry.area {
+            print(countryArea)
+            region = MKCoordinateRegion(center: pointAnnotation.coordinate, span: MKCoordinateSpan(latitudeDelta: 11, longitudeDelta: 11))
         }
         else {
-            region = MKCoordinateRegion(center: self.pointAnnotation.coordinate, span: MKCoordinateSpan(latitudeDelta: 5.5, longitudeDelta: 5.5))
+            region = MKCoordinateRegion(center: pointAnnotation.coordinate, span: MKCoordinateSpan(latitudeDelta: 5.5, longitudeDelta: 5.5))
         }
         
         self.mapViewOutlet.setRegion(region, animated: true)
-        self.mapViewOutlet.addAnnotation(self.pinAnnotationView.annotation!)
-        
-//        print(theCountry.name)
-//        print(theCountry.capital)
-//        print(theCountry.population)
-//        print(theCountry.latitude)
-//        print(theCountry.longitude)
+        self.mapViewOutlet.addAnnotation(pinAnnotationView.annotation!)
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        self.navigationItem.title = "\(theCountry.name) Detail View"
-        nameLabelOutlet?.text = theCountry.name
-        capitalLabelOutlet?.text = theCountry.capital
-        populationLabelOutlet?.text = String(theCountry.population)
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.navigationItem.title = "\(selectedCountry.name) Detail View"      //Set the title to Countries name
+        nameLabelOutlet?.font = UIFont.boldSystemFont(ofSize: 17)              //Bold the label text
+        /*Custom text using information about the country*/
+        nameLabelOutlet?.text = "\(selectedCountry.name)'s population is \(NumberFormatter.localizedString(from: NSNumber(value: selectedCountry.population), number: NumberFormatter.Style.decimal)) and it's capital city is \(selectedCountry.capital)"
     }
 }
--- ebayProgrammingExercise/ViewController.swift
+++ ebayProgrammingExercise/ViewController.swift
@@ -11,54 +11,54 @@ import Foundation
 
 class CountriesTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var countriesArray: [Country] = []
-    //var latitude = 0.0
-    //var longitude = 0.0
-    var didParse = false
+    var countriesArray: [Country] = []           //Global array of custom class Country used to populate table view
+    var didParse = false                         //bool flag so that data is only parsed once
     @IBOutlet var theTableView: UITableView!
-    @IBAction func countryDetailDone(sender: UIStoryboardSegue) {
-        // Intentionally left blank
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.theTableView.reloadData()
+        //self.theTableView.reloadData()
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        self.getAndParseData()
+        self.getAndParseData()                      //call function to get and parse the data from API
+        self.theTableView.reloadData()              //reload the table view once data is obtained
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 0
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.countriesArray.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "countrycell", for: cellForRowAt) as UITableViewCell
-        cell.textLabel?.text = self.countriesArray[cellForRowAt.row].name
+        let cell = tableView.dequeueReusableCell(withIdentifier: "countrycell", for: cellForRowAt) as UITableViewCell       //dequeue a cell
+        cell.textLabel?.text = self.countriesArray[cellForRowAt.row].name     //get country name from countriesArray
         return cell
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "CountryDetailSegue" {
-            guard let indexPath = theTableView.indexPathForSelectedRow else {
-                print("This didn't work")
-                return
+        if segue.identifier == "CountryDetailSegue" {       //check for proper segue
+            guard let indexPath = theTableView.indexPathForSelectedRow else {       //ensure we have index path
+                return          //if no index path return out of function
             }
-            print(indexPath)
-            print(self.countriesArray[indexPath.item].name)
-            print(self.countriesArray[indexPath.item].capital)
-            print(String(self.countriesArray[indexPath.row].population))
+            /*create create instance of destination view controller and sets the selected country variable to the country that was pressed*/
             let countryDetailViewController = segue.destination as! CountryDetailViewController
-            countryDetailViewController.theCountry = self.countriesArray[indexPath.row]
+            countryDetailViewController.selectedCountry = self.countriesArray[indexPath.row]
+            
             theTableView.deselectRow(at: indexPath, animated: true)
         }
     }
+/***************************************************************************************************************
+** getAndParseData() function creates a URL Session configures it with appropriate headers for authentication
+** it then gets the data from API and parses the data into arguments used to call the Country initializer.
+** That created object is then stored in our global countriesArray.
+***************************************************************************************************************/
     func getAndParseData() {
-        if (didParse) {
+        if (didParse) {         //if data has been parsed already return out of function
             return
         }
         else {
@@ -66,6 +66,7 @@ class CountriesTableViewController: UIViewController, UITableViewDataSource, UIT
             
             if let url = URL(string: urlString) {
                 let sessionConfig = URLSessionConfiguration.default
+                /*Create and set headers array for authentication*/
                 let headers = [
                     "X-Mashape-Key":"1IosQYQKu0mshuIZjcqiIXbiLGJSp1dBB9Yjsnfd2aISWLA7Yk",
                     "Accept":"application/json"
@@ -76,18 +77,20 @@ class CountriesTableViewController: UIViewController, UITableViewDataSource, UIT
                 urlSession.dataTask(with: url, completionHandler: { data, response, error in
                     if error == nil && data != nil {
                         let json = try? JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
-                        print(json)
                         if let dataArray = json as? [Any] {
                             var count: Int
                             var latitude: Double
                             var longitude: Double
                             for case let dataDict as [String: Any] in dataArray {
+                                /*reset variables for each countries set of data*/
                                 count = 0
                                 latitude = 0.0
                                 longitude = 0.0
+                                /*get appropriate data from dictionary*/
                                 if let name = dataDict["name"], let capital = dataDict["capital"], let coordinates = dataDict["latlng"], let population = dataDict["population"]{
                                     let area = dataDict["area"]
                                     if let latLongArray = coordinates as? [Any] {
+                                        /*latLong comes back as array with latitudes in even locations and longitudes in odd so assign them thusly*/
                                         for latLong in latLongArray {
                                             if (count % 2 == 0) {
                                                 latitude = latLong as! Double
@@ -98,13 +101,13 @@ class CountriesTableViewController: UIViewController, UITableViewDataSource, UIT
                                             }
                                         }
                                     }
+                                    /*create Country with appropriate arguments and store in countriesArray*/
                                     self.countriesArray.append(Country(name: name as! String, capital: capital as! String, population: population as! Int, area: area as? Double, latitude: latitude, longitude: longitude))
                                 }
                             }
                         }
                         self.theTableView.reloadData()
                         self.didParse = true
-                        print("The country array count is: ", self.countriesArray.count)
                     }
                 }).resume()
             }
